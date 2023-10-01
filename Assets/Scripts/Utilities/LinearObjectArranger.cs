84202b5d6bde8c82f1c70bbf7529cdda3b7beb3f
Aryazaky <aryazakyazif@gmail.com>
2023-10-02 00:47:41 +0700

    added LinearObjectArranger

---
+++ Assets/Scripts/Utilities/LinearObjectArranger.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+
+namespace Gespell.Utilities
+{
+    public class LinearObjectArranger : MonoBehaviour
+    {
+        [SerializeField] private Vector3 startVector = new Vector3(0, 0, 0); // Starting vector for arrangement
+        [SerializeField] private Vector3 endVector = new Vector3(10, 0, 0); // Ending vector for arrangement
+
+        private void Start()
+        {
+            ArrangeChildrenEvenly();
+        }
+
+        private void ArrangeChildrenEvenly()
+        {
+            int childCount = transform.childCount;
+            if (childCount <= 1)
+            {
+                // Nothing to arrange if there is only one child or none.
+                return;
+            }
+
+            float step = 1f / (childCount - 1);
+            for (int i = 0; i < childCount; i++)
+            {
+                Transform child = transform.GetChild(i);
+                float lerpFactor = step * i;
+                Vector3 newPosition = Vector3.Lerp(startVector, endVector, lerpFactor);
+                child.position = newPosition;
+                child.GetComponent<SpriteRenderer>().sortingOrder = i;
+            }
+        }
+
+        private void OnValidate()
+        {
+            ArrangeChildrenEvenly();
+        }
+    }
+}
